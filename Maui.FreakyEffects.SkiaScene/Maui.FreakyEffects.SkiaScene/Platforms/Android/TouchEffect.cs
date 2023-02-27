5ffec790b2da50164e74ea09a7fad488baee83cd
FreakyAli <31090457+FreakyAli@users.noreply.github.com>
2023-02-27 10:13:16 +0530

    fix: code clean up

---
+++ Maui.FreakyEffects.SkiaScene/Maui.FreakyEffects.SkiaScene/Platforms/Android/TouchEffect.cs
@@ -0,0 +1,47 @@
+﻿using Maui.FreakyEffects.TouchTracking;
+using View = Android.Views.View;
+
+namespace Maui.FreakyEffects.Platforms.Android;
+
+public class TouchEffect : Microsoft.Maui.Controls.Platform.PlatformEffect
+{
+    private TouchHandler _touchHandler;
+    private View _view;
+    private Maui.FreakyEffects.TouchTracking.TouchEffect _touchEffect;
+
+    protected override void OnAttached()
+    {
+        _view = Control == null ? Container : Control;
+
+        // Get access to the TouchEffect class in the PCL
+        _touchEffect =
+            (Maui.FreakyEffects.TouchTracking.TouchEffect)Element.Effects.FirstOrDefault(e => e is Maui.FreakyEffects.TouchTracking.TouchEffect);
+
+        if (_touchEffect == null)
+        {
+            return;
+        }
+
+        _touchHandler = new TouchHandler();
+        _touchHandler.TouchAction += TouchHandlerOnTouch;
+        _touchHandler.Capture = _touchEffect.Capture;
+        _touchHandler.RegisterEvents(_view);
+
+    }
+
+    private void TouchHandlerOnTouch(object sender, TouchActionEventArgs args)
+    {
+        _touchEffect.OnTouchAction(sender, args);
+    }
+
+    protected override void OnDetached()
+    {
+        if (_touchHandler == null)
+        {
+            return;
+        }
+        _touchHandler.TouchAction -= TouchHandlerOnTouch;
+        _touchHandler.UnregisterEvents(_view);
+    }
+}
+
--- Maui.FreakyEffects.SkiaScene/Maui.FreakyEffects.SkiaScene/Platforms/Android/TouchHandler.cs
+++ Maui.FreakyEffects.SkiaScene/Maui.FreakyEffects.SkiaScene/Platforms/Android/TouchHandler.cs
@@ -1,83 +1,8 @@
-﻿using Android.Content;
-using Android.Util;
-using Android.Views;
+﻿using Android.Views;
 using Maui.FreakyEffects.TouchTracking;
 using View = Android.Views.View;
 
 namespace Maui.FreakyEffects.Platforms.Android;
-
-public class TouchEffect : Microsoft.Maui.Controls.Platform.PlatformEffect
-{
-    private TouchHandler _touchHandler;
-    private View _view;
-    private Maui.FreakyEffects.TouchTracking.TouchEffect _touchEffect;
-
-    protected override void OnAttached()
-    {
-        _view = Control == null ? Container : Control;
-
-        // Get access to the TouchEffect class in the PCL
-        _touchEffect =
-            (Maui.FreakyEffects.TouchTracking.TouchEffect)Element.Effects.FirstOrDefault(e => e is Maui.FreakyEffects.TouchTracking.TouchEffect);
-
-        if (_touchEffect == null)
-        {
-            return;
-        }
-
-        _touchHandler = new TouchHandler();
-        _touchHandler.TouchAction += TouchHandlerOnTouch;
-        _touchHandler.Capture = _touchEffect.Capture;
-        _touchHandler.RegisterEvents(_view);
-
-    }
-
-    private void TouchHandlerOnTouch(object sender, TouchActionEventArgs args)
-    {
-        _touchEffect.OnTouchAction(sender, args);
-    }
-
-    protected override void OnDetached()
-    {
-        if (_touchHandler == null)
-        {
-            return;
-        }
-        _touchHandler.TouchAction -= TouchHandlerOnTouch;
-        _touchHandler.UnregisterEvents(_view);
-    }
-}
-
-public static class ContextExtensions
-{
-    static float _displayDensity = float.MinValue;
-
-    public static double FromPixels(this Context self, double pixels)
-    {
-        SetupMetrics(self);
-        return pixels / _displayDensity;
-    }
-
-    static void SetupMetrics(Context context)
-    {
-        if (_displayDensity != float.MinValue)
-        {
-            return;
-        }
-
-        using (DisplayMetrics metrics = context.Resources.DisplayMetrics)
-        {
-            _displayDensity = metrics.Density;
-        }
-    }
-
-    public static float ToPixels(this Context self, double dp)
-    {
-        SetupMetrics(self);
-
-        return (float)Math.Round(dp * _displayDensity);
-    }
-}
 
 public class TouchHandler : TouchHandlerBase<View>
 {
