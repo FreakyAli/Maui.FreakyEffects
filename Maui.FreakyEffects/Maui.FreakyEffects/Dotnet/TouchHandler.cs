7982c977c12e8a77e3d9340ae2581f8f9a4909bd
TheresasAccount <50762188+TheresasAccount@users.noreply.github.com>
2024-04-13 04:10:59 -0400

    Adding in .Net stubs so it will compile for .net 8 (#15)
    Co-authored-by: Theresa Walsh <Theresa.Walsh@bentley.com>
    Co-authored-by: Gulam Ali H <hakimgulamali88@gmail.com>

---
+++ Maui.FreakyEffects/Maui.FreakyEffects/Dotnet/TouchHandler.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Maui.Controls.Platform;
+
+namespace Maui.FreakyEffects.Platforms.Dotnet;
+
+internal class TouchEffectPlatform : PlatformEffect
+{
+    protected override void OnAttached()
+    {
+    }
+
+    protected override void OnDetached()
+    {
+    }
+}
