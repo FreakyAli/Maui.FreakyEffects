292d8299ca10da7d97981a0322f26aae172c4f06
FreakyAli <hakimgulamali88@gmail.com>
2024-05-10 19:40:18 +0530

    fix: added some changes

---
+++ Maui.FreakyEffects/Maui.FreakyEffects/Dotnet/TouchHandler.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Maui.Controls.Platform;
+
+namespace Maui.FreakyEffects.Platforms.Dotnet;
+
+internal class TouchEffectPlatform : PlatformEffect
+{
+    protected override void OnAttached()
+    {
+        throw new NotImplementedException();
+    }
+
+    protected override void OnDetached()
+    {
+        throw new NotImplementedException();
+    }
+}
