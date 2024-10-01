2804bf34cba18a035c7e9b57edd69ea6d4ef6f6d
Jon2G <jonyjouna@gmail.com>
2024-10-01 00:59:13 -0600

    Use ClearValue instead of setting color to null
    As @PureWeen suggested calling "ClearValue" instead of setting property to null seems to work.
    https://github.com/dotnet/maui/issues/22914#issuecomment-2365290936

--- Maui.FreakyEffects/Maui.FreakyEffects/Shared/Skeleton/SkeletonEffect.cs
+++ Maui.FreakyEffects/Maui.FreakyEffects/Shared/Skeleton/SkeletonEffect.cs
@@ -6,31 +6,31 @@ public static class SkeletonEffect
 {
     #region Public Properties
 
-    public static readonly BindableProperty IsParentProperty = BindableProperty.CreateAttached("IsParent", typeof(bool), typeof(View), false);
+    public static readonly BindableProperty IsParentProperty = BindableProperty.CreateAttached("IsParent", typeof(bool), typeof(VisualElement), false);
 
     public static void SetIsParent(BindableObject b, bool value) => b.SetValue(IsParentProperty, value);
 
     public static bool GetIsParent(BindableObject b) => (bool)b.GetValue(IsParentProperty);
 
-    public static readonly BindableProperty IsBusyProperty = BindableProperty.CreateAttached("IsBusy", typeof(bool), typeof(View), default(bool), propertyChanged: (b, oldValue, newValue) => OnIsBusyChanged(b, (bool)newValue));
+    public static readonly BindableProperty IsBusyProperty = BindableProperty.CreateAttached("IsBusy", typeof(bool), typeof(VisualElement), default(bool), propertyChanged: (b, oldValue, newValue) => OnIsBusyChanged(b, (bool)newValue));
 
     public static void SetIsBusy(BindableObject b, bool value) => b.SetValue(IsBusyProperty, value);
 
     public static bool GetIsBusy(BindableObject b) => (bool)b.GetValue(IsBusyProperty);
 
-    public static readonly BindableProperty HideProperty = BindableProperty.CreateAttached("Hide", typeof(bool), typeof(View), default(bool));
+    public static readonly BindableProperty HideProperty = BindableProperty.CreateAttached("Hide", typeof(bool), typeof(VisualElement), default(bool));
 
     public static void SetHide(BindableObject b, bool value) => b.SetValue(HideProperty, value);
 
     public static bool GetHide(BindableObject b) => (bool)b.GetValue(HideProperty);
 
-    public static readonly BindableProperty BackgroundColorProperty = BindableProperty.CreateAttached("BackgroundColor", typeof(Color), typeof(View), default(Color), propertyChanged: (b, oldValue, newValue) => OnIsBusyChanged(b, GetIsBusy(b)));
+    public static readonly BindableProperty BackgroundColorProperty = BindableProperty.CreateAttached("BackgroundColor", typeof(Color), typeof(VisualElement), default(Color), propertyChanged: (b, oldValue, newValue) => OnIsBusyChanged(b, GetIsBusy(b)));
 
     public static void SetBackgroundColor(BindableObject b, Color value) => b.SetValue(BackgroundColorProperty, value);
 
     public static Color GetBackgroundColor(BindableObject b) => (Color)b.GetValue(BackgroundColorProperty);
 
-    public static readonly BindableProperty AnimationProperty = BindableProperty.CreateAttached("Animation", typeof(BaseAnimation), typeof(View), null, propertyChanged: (b, oldValue, newValue) => OnIsBusyChanged(b, GetIsBusy(b)));
+    public static readonly BindableProperty AnimationProperty = BindableProperty.CreateAttached("Animation", typeof(BaseAnimation), typeof(VisualElement), null, propertyChanged: (b, oldValue, newValue) => OnIsBusyChanged(b, GetIsBusy(b)));
 
     public static void SetAnimation(BindableObject b, BaseAnimation value) => b.SetValue(AnimationProperty, value);
 
@@ -40,7 +40,7 @@ public static class SkeletonEffect
 
     #region Internal Properties
 
-    internal static readonly BindableProperty AnimatingProperty = BindableProperty.CreateAttached("Animating", typeof(bool), typeof(View), default(bool));
+    internal static readonly BindableProperty AnimatingProperty = BindableProperty.CreateAttached("Animating", typeof(bool), typeof(VisualElement), default(bool));
 
     [TypeConverter(typeof(BaseAnimationTypeConverter))]
     internal static void SetAnimating(BindableObject b, bool value) => b.SetValue(AnimatingProperty, value);
@@ -48,33 +48,54 @@ public static class SkeletonEffect
     [TypeConverter(typeof(BaseAnimationTypeConverter))]
     internal static bool GetAnimating(BindableObject b) => (bool)b.GetValue(AnimatingProperty);
 
-    internal static readonly BindableProperty CancelAnimationProperty = BindableProperty.CreateAttached("CancelAnimation", typeof(bool), typeof(View), default(bool));
+    internal static readonly BindableProperty CancelAnimationProperty = BindableProperty.CreateAttached("CancelAnimation", typeof(bool), typeof(VisualElement), default(bool));
 
     internal static void SetCancelAnimation(BindableObject b, bool value) => b.SetValue(CancelAnimationProperty, value);
 
     internal static bool GetCancelAnimation(BindableObject b) => (bool)b.GetValue(CancelAnimationProperty);
 
-    internal static readonly BindableProperty OriginalBackgroundColorProperty = BindableProperty.CreateAttached("OriginalBackgroundColor", typeof(Color), typeof(View), default(Color));
+    internal static readonly BindableProperty OriginalBackgroundColorProperty = BindableProperty.CreateAttached("OriginalBackgroundColor", typeof(Color), typeof(VisualElement), default(Color));
 
-    internal static void SetOriginalBackgroundColor(BindableObject b, Color value) => b.SetValue(OriginalBackgroundColorProperty, value);
+    internal static void SetOriginalBackgroundColor(BindableObject b, Color value)
+    {
+        if (value is not null)
+        {
+            b.SetValue(OriginalBackgroundColorProperty, value);
+        }
+        else
+        {
+            b.ClearValue(OriginalBackgroundColorProperty);
+        }
+    }
+
 
     internal static Color GetOriginalBackgroundColor(BindableObject b) => (Color)b.GetValue(OriginalBackgroundColorProperty);
 
-    internal static readonly BindableProperty UseDynamicTextColorProperty = BindableProperty.CreateAttached("UseDynamicTextColor", typeof(bool), typeof(View), default(bool));
+    internal static readonly BindableProperty UseDynamicTextColorProperty = BindableProperty.CreateAttached("UseDynamicTextColor", typeof(bool), typeof(VisualElement), default(bool));
 
     internal static void SetUseDynamicTextColor(BindableObject b, bool value) => b.SetValue(UseDynamicTextColorProperty, value);
 
     internal static bool GetUseDynamicTextColor(BindableObject b) => (bool)b.GetValue(UseDynamicTextColorProperty);
 
-    internal static readonly BindableProperty UseDynamicBackgroundColorProperty = BindableProperty.CreateAttached("UseDynamicBackground", typeof(bool), typeof(View), default(bool));
+    internal static readonly BindableProperty UseDynamicBackgroundColorProperty = BindableProperty.CreateAttached("UseDynamicBackground", typeof(bool), typeof(VisualElement), default(bool));
 
     internal static bool GetUseDynamicBackgroundColor(BindableObject b) => (bool)b.GetValue(UseDynamicBackgroundColorProperty);
 
     internal static void SetUseDynamicBackgroundColor(BindableObject b, bool value) => b.SetValue(UseDynamicBackgroundColorProperty, value);
 
-    internal static readonly BindableProperty OriginalTextColorProperty = BindableProperty.CreateAttached("OriginalTextColor", typeof(Color), typeof(View), default(Color));
+    internal static readonly BindableProperty OriginalTextColorProperty = BindableProperty.CreateAttached("OriginalTextColor", typeof(Color), typeof(VisualElement), default(Color));
 
-    internal static void SetOriginalTextColor(BindableObject b, Color value) => b.SetValue(OriginalTextColorProperty, value);
+    internal static void SetOriginalTextColor(BindableObject b, Color value)
+    {
+        if (value is not null)
+        {
+            b.SetValue(OriginalTextColorProperty, value);
+        }
+        else
+        {
+            b.ClearValue(OriginalTextColorProperty);
+        }
+    }
 
     internal static Color GetOriginalTextColor(BindableObject b) => (Color)b.GetValue(OriginalTextColorProperty);
 
@@ -84,7 +105,7 @@ public static class SkeletonEffect
 
     private static void OnIsBusyChanged(BindableObject bindable, bool newValue)
     {
-        if (bindable.GetType().IsSubclassOf(typeof(View)))
+        if (bindable.GetType().IsSubclassOf(typeof(VisualElement)))
         {
             HandleIsBusyChanged(bindable, newValue);
         }
@@ -165,12 +186,11 @@ public static class SkeletonEffect
     private static void SetBackgroundColor(View view)
     {
         var hasDynamic = GetUseDynamicBackgroundColor(view)
-            || view.HasDynamicColorOnProperty(View.BackgroundColorProperty);
-
+            || view.HasDynamicColorOnProperty(VisualElement.BackgroundColorProperty);
+        SetOriginalBackgroundColor(view, view.BackgroundColor);
         var backgroundColor = GetBackgroundColor(view);
         if (backgroundColor != default(Color))
         {
-            SetOriginalBackgroundColor(view, view.BackgroundColor);
             view.BackgroundColor = backgroundColor;
         }
 
@@ -180,14 +200,21 @@ public static class SkeletonEffect
     private static void RestoreBackgroundColor(View view)
     {
         var useDynamic = GetUseDynamicBackgroundColor(view);
-        var backgroundColor = GetBackgroundColor(view);
         if (useDynamic)
         {
-            view.ClearValue(View.BackgroundColorProperty);
+            var key = view.GetPropertyDynamicResourceKey(VisualElement.BackgroundColorProperty);
+            view.SetDynamicResource(VisualElement.BackgroundColorProperty, key);
         }
-        else if (backgroundColor != default(Color))
+        else
         {
-            view.BackgroundColor = GetOriginalBackgroundColor(view) ?? Colors.Transparent;
+            if (GetOriginalBackgroundColor(view) is { } originalColor)
+            {
+                view.BackgroundColor = originalColor;
+            }
+            else
+            {
+                view.ClearValue(VisualElement.BackgroundColorProperty);
+            }
         }
     }
 
@@ -220,9 +247,13 @@ public static class SkeletonEffect
                 var key = label.GetPropertyDynamicResourceKey(Label.TextColorProperty);
                 label.SetDynamicResource(Label.TextColorProperty, key);
             }
+            else if (GetOriginalTextColor(view) is { } originalTextColor)
+            {
+                label.TextColor = originalTextColor;
+            }
             else
             {
-                label.TextColor = GetOriginalTextColor(view);
+                label.ClearValue(Label.TextColorProperty);
             }
         }
         else if (view is Button button)
@@ -232,9 +263,13 @@ public static class SkeletonEffect
                 var key = button.GetPropertyDynamicResourceKey(Button.TextColorProperty);
                 button.SetDynamicResource(Button.TextColorProperty, key);
             }
+            else if (GetOriginalTextColor(view) is { } originalTextColor)
+            {
+                button.TextColor = originalTextColor;
+            }
             else
             {
-                button.TextColor = GetOriginalTextColor(view);
+                button.ClearValue(Button.TextColorProperty);
             }
         }
     }
